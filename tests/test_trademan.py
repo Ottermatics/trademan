36ac6b36d78778dedb08213f985e29615dde413d
SoundsSerious <keavin7@ottermatics.com>
2025-09-09 17:06:11 -0400

    Refactor code formatting and improve consistency across multiple files

--- tests/test_trademan.py
+++ tests/test_trademan.py
@@ -11,14 +11,19 @@ import os
 
 import subprocess as subp
 
+
 class TestTrademan(unittest.TestCase):
     """Tests for `trademan` package."""
 
     def test_market_dl(self):
         """gets appl"""
-        dl = data.get_tickers('AAPL')
-        perf = data.data_db[f'perf/AAPL']
+        dl = data.get_tickers("AAPL")
+        perf = data.data_db[f"perf/AAPL"]
 
-    def test_cli(self) :
-        os.system("""trademan -cls etfs -gamma 1 -alloc 100000 -in QQQ,SCHG,VGT,SLV,VIG,SPY,VOO,VUG,IAU,PAVE,NANC""")
-        os.system("""trademan -cls etfs -gamma 20 -alloc 100000 -in QQQ,SCHG,VGT,SLV,VIG,SPY,VOO,VUG,IAU,PAVE,NANC,KRUZ -cycl-err 100""")
+    def test_cli(self):
+        os.system(
+            """trademan -cls etfs -gamma 1 -alloc 100000 -in QQQ,SCHG,VGT,SLV,VIG,SPY,VOO,VUG,IAU,PAVE,NANC"""
+        )
+        os.system(
+            """trademan -cls etfs -gamma 20 -alloc 100000 -in QQQ,SCHG,VGT,SLV,VIG,SPY,VOO,VUG,IAU,PAVE,NANC,KRUZ -cycl-err 100"""
+        )
